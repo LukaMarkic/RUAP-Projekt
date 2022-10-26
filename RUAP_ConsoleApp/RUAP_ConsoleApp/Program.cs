285642e044d261615359be14710f1e19471ade2c
Luka Markic <lukamarkiclm@gmail.com>
2022-10-26 15:47:32 +0200

    Prvi commit.

---
+++ RUAP_ConsoleApp/RUAP_ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RUAP_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Luke Markića!");
+        }
+    }
+}
